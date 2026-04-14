81aa2f78945bfa3af2848beed4677e478cc4323d
NimeshKolambage <nimeshkolambage@gmail.com>
2026-04-14 18:09:33 +0530

    update

--- server/server.js
+++ server/server.js
@@ -7,3 +7,43 @@ const app = express();
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
+
+const API_KEY = process.env.API_KEY;
+
+function fetchNews(Url,res) {
+    axios.get(Url)
+       .then(response => {
+
+        if(response.data.totalResults > 0) {
+
+            res.json({
+                status:200,
+                success: true,
+                message: "News fetched successfully",
+                data:response.data,
+                articles: response.data.articles
+            });
+
+        } else {
+            res.status(404).json({
+                status: 404,
+                message: "No news found"
+            });
+        }
+    })
+    .catch((error) => {
+        console.error("Error fetching news:", error);
+        res.status(500).json({
+            status: 500,
+            message: "Failed to fetch news"
+        });
+    });
+}
+
+app.get("/all-news",(req,res)=>{
+
+    let pageSize = parseInt(req.query.pageSize) || 10;
+    let page = parseInt(req.query.page) || 1;
+    let url=`https://newsapi.org/v2/everything?q=page=${page}&pageSize=${pageSize}&apiKey=${API_KEY}`; 
+    fetchNews(url,res); 
+})
